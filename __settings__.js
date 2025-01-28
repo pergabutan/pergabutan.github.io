514a5a416fa8af82a9cec436f78357477dfa3042
Azzis Arswendo <azzis@hexacode.org>
2025-01-28 17:29:53 +0700

    Initial commit from Create PlayCanvas

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2159073.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 212273065, 212273069, 212273072, 212273064, 212273091 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/212273075/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/212273275/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/212273068/1/ammo.js', 'preload' : true},
+];
